748a2cf132cdfd315a9ec66db16694f1c35e9da7
Andrew Ballinger <aballinger@payjunction.com>
2016-08-15 15:17:12 -0700

    Now we have two views\!

--- madlibsApp.js
+++ madlibsApp.js
@@ -1,5 +1,5 @@
 angular.module('MadlibsApp', [])
-  .controller('MadlibsCtrl', function(){
+  .controller('MadlibsCtrl', function($scope){
      var ml = this;
 
      ml.fields = {
@@ -8,32 +8,38 @@ angular.module('MadlibsApp', [])
       'obnoxious_celebrity' : {  ph: 'Obnoxious celebrity', value: ''},
       'job_title' : { ph: 'Job title', value: ''},
       'celebrity' : { ph: 'Celebrity', value: ''},
-      'huge_number' : { ph: 'Huge number', value: ''},
+      'huge_number' : { ph: 'Huge number', value: '', type: "number" },
       'tedious_task' : { ph: 'Tedious task', value: ''},
       'useless_skill' : { ph: 'Useless skill', value: ''},
       'adjective': { ph: 'Adjective', value: ''}
     }
 
-   ml.gender_words = [
-      ["he", "him", "himself", "his", "his"],
-      ["she", "her", "herself", "hers", "her"],
-      ["it", "it", "itself", "its", "its"],
-      ["ey", "em", "emself", "eirs", "eir"]
+   ml.genders = [
+      { "name": "Male"   , subj: "he",  obj: "him", reflx: "himself", poss: "his",  poss2: "his" },
+      { "name": "Female" , subj: "she", obj: "her", reflx: "herself", poss: "hers", poss2: "her" },
+      { "name": "Neuter" , subj: "it",  obj: "it",  reflx: "itself",  poss: "its",  poss2: "its" },
+      { "name": "Neutral", subj: "ey",  obj: "em",  reflx: "emself",  poss: "eirs", poss2: "eir" }
     ]
+  
+   ml.gender = ml.genders[0];
+   ml.view = 'form';
 
-    ml.gender_index = 1;
+   ml.submit = function() {
+     ml.view = 'madlib';
+   }
 
-    [ ml.subjective, 
-      ml.objective, 
-      ml.reflexive, 
-      ml.possessive,
-      ml.possessive2 ] = ml.gender_words[0]
 
-    ml.genderSwap = function(){
-      [ ml.subjective, 
-        ml.objective, 
-        ml.reflexive, 
-        ml.possessive,
-        ml.possessive2 ] = ml.gender_words[ml.gender_index++ % ml.gender_words.length];
-    }
+   ml.reset = function() {
+     ml.view = 'form';
+     ml.fields = { 'name' : { ph: 'name', value: ''},
+      'dirty_task' : { ph: 'Dirty Task', value: ''},
+      'obnoxious_celebrity' : {  ph: 'Obnoxious celebrity', value: ''},
+      'job_title' : { ph: 'Job title', value: ''},
+      'celebrity' : { ph: 'Celebrity', value: ''},
+      'huge_number' : { ph: 'Huge number', value: '', type: "number" },
+      'tedious_task' : { ph: 'Tedious task', value: ''},
+      'useless_skill' : { ph: 'Useless skill', value: ''},
+      'adjective': { ph: 'Adjective', value: ''}}
+   }
+
   });
