77883202fbd0666215a9f2a298ec1367b13cd77d
Andrew Ballinger <aballinger@payjunction.com>
2016-08-15 12:37:28 -0700

    Adjust gender button

--- madlibsApp.js
+++ madlibsApp.js
@@ -1,7 +1,8 @@
 angular.module('madlibsApp', [])
   .controller('madlibsCtrl', function($scope){
-    $scope.fields = {
-      'male_name' : { ph: 'male name', value: ''},
+
+     $scope.fields = {
+      'name' : { ph: 'name', value: ''},
       'dirty_task' : { ph: 'Dirty Task', value: ''},
       'obnoxious_celebrity' : {  ph: 'Obnoxious celebrity', value: ''},
       'job_title' : { ph: 'Job title', value: ''},
@@ -10,5 +11,28 @@ angular.module('madlibsApp', [])
       'tedious_task' : { ph: 'Tedious task', value: ''},
       'useless_skill' : { ph: 'Useless skill', value: ''},
       'adjective': { ph: 'Adjective', value: ''}
+    }
+
+   $scope.gender_words = [
+      ["he", "him", "himself", "his", "his"],
+      ["she", "her", "herself", "hers", "her"],
+      ["it", "it", "itself", "its", "its"],
+      ["ey", "em", "emself", "eirs", "eir"]
+    ]
+
+    $scope.gender_index = 1;
+
+    [$scope.subjective, 
+     $scope.objective, 
+     $scope.reflexive, 
+     $scope.possessive
+     $scope.possessive2 ] = $scope.gender_words[0]
+
+    $scope.genderSwap = function(){
+      [$scope.subjective, 
+       $scope.objective, 
+       $scope.reflexive, 
+       $scope.possessive,
+       $scope.possessive2] = $scope.gender_words[$scope.gender_index++ % $scope.gender_words.length];
     }
   });
